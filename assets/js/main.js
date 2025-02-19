034b0658e15af7dee26c2de06ef27fe62d0d8b54
Filipe Teixeira <filipebarrosteixeira98@gmail.com>
2025-02-19 15:35:09 -0300

    chore: add blur effect to header element

--- assets/js/main.js
+++ assets/js/main.js
@@ -23,3 +23,11 @@ const handleLinksAction = () => {
 }
 
 navigationLinks.forEach((navigationLink) => navigationLink.addEventListener('click', handleLinksAction))
+
+const handleBlurHeader = () => {
+  const header = document.getElementById('header')
+
+  this.scrollY >= 50 ? header.classList.add('blur-header') : header.classList.remove('blur-header')
+}
+
+window.addEventListener('scroll', handleBlurHeader)
