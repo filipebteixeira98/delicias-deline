84ed66f5f9a316f6284a0045b858bfcf995c1572
Filipe Teixeira <filipebarrosteixeira98@gmail.com>
2025-02-20 17:44:05 -0300

    chore: add styles to selected link of section which user is at

--- assets/js/main.js
+++ assets/js/main.js
@@ -39,3 +39,24 @@ const handleScrollUp = () => {
 }
 
 window.addEventListener('scroll', handleScrollUp)
+
+const sections = document.querySelectorAll('section[id]')
+
+const handleScrollToSection = () => {
+  const scrollDown = window.scrollY
+
+  sections.forEach((section) => {
+    const sectionHeight = section.offsetHeight,
+      sectionTop = section.offsetTop - 58,
+      sectionId = section.getAttribute('id'),
+      sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']')
+
+    if (scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight) {
+      sectionsClass.classList.add('active-link')
+    } else {
+      sectionsClass.classList.remove('active-link')
+    }
+  })
+}
+
+window.addEventListener('scroll', handleScrollToSection)
